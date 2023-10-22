e2da30ae9052d74ef5054c1f8a7c58210199454b
Lahtisen-Purkkaviritykset <145877275+Lahtisen-Purkkaviritykset@users.noreply.github.com>
2023-10-22 20:06:15 +0300

    Aloitus
    lisätään scanneri

--- src/Laivanupotus.java
+++ src/Laivanupotus.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
 
 public class Laivanupotus {
 
-	public static void main(String[] args) {
-	
+    public static void main(String[] args) {
+        int koko = 5;
+        char[][] pelilauta = new char[koko][koko];
 
+        alustaPelilauta(pelilauta);
+
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            piirraPelilauta(pelilauta);
+            int rivi = kysyKoordinaatti("Rivi", koko);
+            if (rivi == 0) {
+                break;
+            }
+
+            int sarake = kysyKoordinaatti("Sarake", koko);
+
+            pelilauta[rivi - 1][sarake - 1] = 'X'; // Ammutut näkyvät "X" kun pelilauta piirretään uudestaan
+
+        } while (true);
+
+        System.out.println("Peli päättyi.");
+        scanner.close();
 	}
 
 }
