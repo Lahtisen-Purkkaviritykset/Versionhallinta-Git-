8caee74c6eb8da107c9b208ae8a43e3e4711ce54
Lahtisen-Purkkaviritykset <145877275+Lahtisen-Purkkaviritykset@users.noreply.github.com>
2023-10-22 20:09:27 +0300

    Pelaajan kordinaatit
    Kysytään pelaajan koordinaatit

--- src/Laivanupotus.java
+++ src/Laivanupotus.java
@@ -37,4 +37,36 @@ public class Laivanupotus {
             	}
         	}
     	}
+    // Piirretään pelilauta
+    private static void piirraPelilauta(char[][] pelilauta) 
+    {
+        System.out.println("  1 2 3 4 5");
+        for (int i = 0; i < pelilauta.length; i++) 
+        	{
+            System.out.print((i + 1) + " ");
+            for (int j = 0; j < pelilauta[i].length; j++) 
+            	{
+                System.out.print(pelilauta[i][j] + " ");
+            	}
+            System.out.println();
+        	}
+    }
+    // Kysytään pelaajalta koordinaatteja, syöttämällä "0", peli loppuu
+    private static int kysyKoordinaatti(String koordinaatti, int koko) {
+        Scanner scanner = new Scanner(System.in);
+        int arvo;
+        do {
+            System.out.print(koordinaatti + " (1-" + koko + ", 0 lopettaa): ");
+            while (!scanner.hasNextInt()) {
+                System.out.println("Virheellinen syöte. Anna kokonaisluku.");
+                scanner.next();
+            }
+            arvo = scanner.nextInt();
+            if (arvo < 0 || arvo > koko) {
+                System.out.println("Virheellinen syöte. Anna arvo väliltä 1-" + koko + ".");
+            }
+        } while (arvo < 0 || arvo > koko);
+        return arvo;
+    }
 }
+
